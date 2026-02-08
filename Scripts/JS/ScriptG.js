ed84b0163ec5348b30de42a73be5b9a486ace514
Renzo <renzelectronica@gmail.com>
2026-02-08 17:13:41 -0500

    Actualización automática 2026-02-08 17:13:40.521145

--- Scripts/JS/ScriptG.js
+++ Scripts/JS/ScriptG.js
@@ -1,3 +1,20 @@
+let header_text1 = document.getElementById("1_1");
+let header_text2 = document.getElementById("1_2");
+let header_text3 = document.getElementById("1_3");
+let header_text4 = document.getElementById("1_4");
+let header_text5 = document.getElementById("1_5");
+switch (window.location) {
+  case "Home.htm":
+    header_text1.style =
+      "color: rgb(0, 0, 0);margin: 13px 13px 13px 13px;background-color: rgb(255, 255, 255);border-radius: 7px;border: rgb(255, 255, 255) solid 2px;";
+    break;
+  case "Cronometer.htm":
+    header_text3.style =
+      "color: rgb(0, 0, 0);margin: 13px 13px 13px 13px;background-color: rgb(255, 255, 255);border-radius: 7px;border: rgb(255, 255, 255) solid 2px;";
+    break;
+  default:
+    break;
+}
 function cronometer() {
   if (window.location != "Home.htm") {
     window.location = "Cronometro.htm";
